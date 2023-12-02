61b6ad5733e172b87497958b3873ef121dafe469
Khaydarov <@khaydaro1>
2023-12-03 01:47:59 +0500

    news_project

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView,\
+    PasswordChangeDoneView,PasswordResetView,PasswordResetConfirmView,PasswordResetDoneView,PasswordResetCompleteView
 from .views import user_login, dashboard_view
 
 urlpatterns = [
     # path('login/', user_login, name='login')
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('password-change', PasswordChangeView.as_view(), name='password_change'),
+    path('password-change-done', PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password-raset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done',PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>',PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('password-reset/complate',PasswordResetCompleteView.as_view(),name='password_reset_complate'),
     path('profile/', dashboard_view, name='user_profile')
 ]
