82e112fce86ee8828f7327b9501f5cfc24a696dd
Khaydarov <@khaydaro1>
2024-01-05 16:53:10 +0500

    news_project

--- news_project/settings.py
+++ news_project/settings.py
@@ -128,3 +128,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home_page'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
